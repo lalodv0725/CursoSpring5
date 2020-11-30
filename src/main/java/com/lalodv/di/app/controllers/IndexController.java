93c5f8c80f7928e372b7c53f382dc45201471673
Lalo DV <lalodv0725@outlook.com>
2020-11-29 20:38:40 -0600

    32.- Inyeccion Dependencias @Autowired

--- src/main/java/com/lalodv/di/app/controllers/IndexController.java
+++ src/main/java/com/lalodv/di/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.lalodv.di.app.controllers;
 
 import com.lalodv.di.app.models.service.MiServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    private MiServicio servicio = new MiServicio();
+    @Autowired
+    private MiServicio servicio;
+    //private MiServicio servicio = new MiServicio();
 
     @GetMapping({"/", "index", "/inicio", ""})
     public String index(Model model){
--- src/main/java/com/lalodv/di/app/models/service/MiServicio.java
+++ src/main/java/com/lalodv/di/app/models/service/MiServicio.java
@@ -1,5 +1,9 @@
 package com.lalodv.di.app.models.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component // @Service tambien se puede usar en lugar de @Component
 public class MiServicio {
     public String operacion(){
         return "ejecutando algun proceso importante...";
