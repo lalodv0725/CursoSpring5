511f856acce4f9d026c580ef48840b4b58e3cc61
Lalo DV <lalodv0725@outlook.com>
2020-12-01 10:52:27 -0600

    37. Anotacion @Qualifier

--- src/main/java/com/lalodv/di/app/controllers/IndexController.java
+++ src/main/java/com/lalodv/di/app/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.lalodv.di.app.controllers;
 import com.lalodv.di.app.models.service.IServicio;
 import com.lalodv.di.app.models.service.MiServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +19,14 @@ public class IndexController {
 //    public IndexController(IServicio servicio) {
 //        this.servicio = servicio;
 //    }
+    @Autowired
+    @Qualifier("MiServicioSimple")
+    private IServicio servicioSimple;
 
     @GetMapping({"/", "index", "/inicio", ""})
     public String index(Model model){
         model.addAttribute("objetoServicio", servicio.operacion());
+        model.addAttribute("objetoServicioSimple", servicioSimple.operacion());
         model.addAttribute("titulo","Pasando datos del servicio(modelo) a la vista");
         return "index";
     }
